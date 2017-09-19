bbfa4dffefd975d7267b1b82ac9efffaa1540378
Zhang Rui <1694246305@qq.com>
2017-09-19 22:35:15 +0800

    提交代码

---
+++ js/init.js
@@ -0,0 +1,39 @@
+function onQueryShippingOrder() {
+	//	var e = document.getElementById("global_number");
+	//	if(e.value == '') {
+	//		return
+	//	}
+	//	var rparams = "/ostrich/queryShippingOrder?";
+	//	rparams += "&global_number=" + e.value;
+	//	myajax("get", rparams, "", onQeryShippingOrderInfoResponse, "application/x-www-form-urlencoded");
+	var res = {
+		"global_number": "S005002132",
+		"year_mark": "2017",
+		"number": "Lg666",
+		"accept_date": "2017-07-31",
+		"working_time": "2017-08-10",
+		"expect_arrived_time": "2017-07-31",
+		"dispach_station_name": "厦门（长沙线）",
+		"dest_station_name": "临泽县",
+		"consignor_name": "KJ008",
+		"consignee_name": "上海鼎盛贸易有限公司",
+		"consignor_contact": "李四",
+		"consignee_contact": "鼎盛总裁",
+		"consignor_phone": "技术顾问",
+		"consignee_phone": "123456456",
+		"consignor_address": "厦门集美大厦",
+		"consignee_address": "上海市人民广场",
+		"goods_name": "书籍",
+		"package_name": "4",
+		"piece": "0",
+		"weight": "0.0000",
+		"volume": "0.0000",
+		"state": "在途",
+		"track_info": ""
+	}
+	var inputs=document.getElementsByTagName('input');
+	debugger;
+//	inputs.map((item)=>{
+//		debugger;
+//	});
+}
